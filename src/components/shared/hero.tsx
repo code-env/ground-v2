de0ccba0cedcd0932ec63ba037b04e4d8623f4ab
code-env <org.codeen@gmail.com>
2024-12-28 00:30:51 +0100

    feat: making some landing page improvement

--- src/components/shared/hero.tsx
+++ src/components/shared/hero.tsx
@@ -2,15 +2,15 @@ import Waitlist from "@/components/forms/waitlist";
 
 const Hero = () => {
   return (
-    <div className="flex flex-col items-center justify-center pt-40 pb-20 gap-10 border-b mx-auto w-full max-w-screen-lg">
+    <div className="flex flex-col items-center justify-center pt-40 pb-20 gap-10 md:border-b mx-auto w-full max-w-screen-lg">
       <div className="size-20">
         <img src="/Graound.svg" alt="Graound" className="size-full" />
       </div>
       <div className="flex flex-col items-center justify-center gap-4 max-w-sm">
-        <h1 className="text-5xl font-medium  text-center leading-tight">
+        <h1 className="md:text-5xl text-2xl font-medium text-center leading-tight">
           Animations build for performance
         </h1>
-        <p className="text-xl text-center">
+        <p className="md:text-xl text-base text-center">
           We're building a library of animations that are built for performance.
         </p>
       </div>
