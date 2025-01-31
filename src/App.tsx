3418760bd9afaa8e482bc96abea133c4c920da81
code-env <org.codeen@gmail.com>
2025-01-31 11:10:38 +0100

    feat: added the years tab

--- src/App.tsx
+++ src/App.tsx
@@ -27,7 +27,7 @@ import NewHero from "./components/animations/animate-text";
 import PeerListBar from "./components/animations/peerlist-bar";
 import TabBars from "./components/animations/tab-bars";
 import JoiDownloadButton from "./components/animations/joi-download-button";
-// import YearsTabs from "./components/animations/years-tabs";
+import YearsTabs from "./components/animations/years-tabs";
 
 const App = () => {
   // const images = ["/succession.jpeg", "/mirror.jpeg", "/dune.jpg"];
@@ -38,9 +38,9 @@ const App = () => {
       {/* <ComponentPreview>
         <Cursor />
       </ComponentPreview> */}
-      {/* <ComponentPreview>
+      <ComponentPreview>
         <YearsTabs />
-      </ComponentPreview> */}
+      </ComponentPreview>
       <ComponentPreview>
         <JoiDownloadButton />
       </ComponentPreview>
