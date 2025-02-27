1bf5329802aa949e19ae75ab41efe4f4d47fea66
code-env <org.codeen@gmail.com>
2025-02-27 17:01:20 +0100

    feat: modified click-select component and now the height is in sync

--- src/components/animations/click-select.tsx
+++ src/components/animations/click-select.tsx
@@ -1,5 +1,10 @@
 import { useState } from "react";
-import { motion as m } from "motion/react";
+import {
+  AnimatePresence,
+  motion as m,
+  MotionConfig,
+  Transition,
+} from "motion/react";
 import { X } from "lucide-react";
 
 type Item = {
@@ -19,6 +24,11 @@ const list: Item[] = [
   ...i,
   id: idx + 1,
 }));
+
+const TRANSITION: Transition = {
+  duration: 0.3,
+  ease: "easeInOut",
+};
 
 const ClickSelect = () => {
   const [selected, setSelected] = useState<Item[]>([]);
@@ -43,45 +53,51 @@ const ClickSelect = () => {
   //   };
 
   return (
-    <div className="size-full bg-background rounded-xl flex items-center justify-center flex-col">
-      <ul className="h-20 border flex items-center gap-2 w-96">
-        {selected.length > 0 &&
-          selected.map((select, idx) => (
-            <m.li
-              key={select.name + idx}
-              layoutId={select.name}
-              className="bg-red-500 text-white flex cursor-pointer items-center gap-4 rounded-full  w-fit p-2 pl-4 capitalize"
-            >
-              <span>{select.name}</span>
-              <button className="size-6 bg-blue-400 rounded-full flex items-center justify-center">
-                <X className="size-4" />
-              </button>
-            </m.li>
-          ))}
-      </ul>
-      <div
-        // animate={{ height: "auto" }}
-        // layoutId="container-items"
-        className="w-96 h-auto gap-4 flex flex-col border p-4 shadow-lg bg-background rounded-xl"
-      >
-        <div className="flex items-center justify-between">
-          <h1>Select filter</h1>
-          <button>Clear</button>
-        </div>
-        <ul className="flex flex-col gap-2">
-          {items.map((item, idx) => (
-            <m.li
-              key={item.name + idx}
-              layoutId={item.name}
-              onClick={() => handleAdd(item)}
-              className="bg-red-500 text-white flex cursor-pointer items-center gap-4 rounded-full  w-fit p-2 capitalize"
-            >
-              <span>{item.name}</span>
-            </m.li>
-          ))}
+    <MotionConfig transition={TRANSITION}>
+      <div className="size-full bg-background rounded-xl flex items-center justify-center flex-col">
+        <ul className="h-20 border flex items-center gap-2 w-96">
+          {selected.length > 0 &&
+            selected.map((select, idx) => (
+              <m.li
+                key={select.name + idx}
+                layoutId={select.name}
+                className="bg-red-500 text-white flex cursor-pointer items-center gap-4 rounded-full  w-fit p-2 pl-4 capitalize"
+              >
+                <span>{select.name}</span>
+                <button className="size-6 bg-blue-400 rounded-full flex items-center justify-center">
+                  <X className="size-4" />
+                </button>
+              </m.li>
+            ))}
         </ul>
+        <m.div
+          animate={{ height: "auto" }}
+          layoutId="container-items"
+          className="w-96 h-auto gap-4 flex flex-col border p-4 shadow-lg bg-background rounded-xl relative overflow-hidden"
+        >
+          <div className="flex items-center justify-between">
+            <m.h1 layoutId="filter-text" className="">
+              Select filter
+            </m.h1>
+            <button>Clear</button>
+          </div>
+          <ul className="flex flex-col gap-2 ">
+            <AnimatePresence mode="popLayout">
+              {items.map((item, idx) => (
+                <m.li
+                  key={item.name + idx}
+                  layoutId={item.name}
+                  onClick={() => handleAdd(item)}
+                  className="bg-red-500 text-white flex cursor-pointer items-center gap-4 rounded-full  w-fit p-2 capitalize z-0 relative"
+                >
+                  <span>{item.name}</span>
+                </m.li>
+              ))}
+            </AnimatePresence>
+          </ul>
+        </m.div>
       </div>
-    </div>
+    </MotionConfig>
   );
 };
 
