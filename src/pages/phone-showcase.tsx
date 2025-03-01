3952018290a8188d23bcda0a31f32bf0e546e3c7
code-env <org.codeen@gmail.com>
2025-03-01 13:14:50 +0100

    feat: finished the phone

--- src/pages/phone-showcase.tsx
+++ src/pages/phone-showcase.tsx
@@ -1,12 +1,14 @@
-import { useState } from "react";
+import Waitlist from "@/components/forms/waitlist";
+import Logo from "@/components/shared/logo";
+import { Button } from "@/components/ui/button";
+import { cn } from "@/lib/utils";
 import {
   AnimatePresence,
   motion as m,
   MotionConfig,
   Transition,
-  Variants,
 } from "motion/react";
-import { cn } from "@/lib/utils";
+import { useState } from "react";
 
 const TRANSITION: Transition = {
   duration: 0.3,
@@ -18,7 +20,6 @@ type Item = {
   description: string;
   id: number;
   angle: number;
-  className?: string;
 };
 
 const items: Item[] = [
@@ -27,28 +28,44 @@ const items: Item[] = [
     description:
       "Start by selecting your current mode-calm, stressed, or in need of focus. Ground curates the perfect for you.",
     angle: -12,
-    // className: "bg-red-500",
   },
   {
     name: "Listen, Breathe, and Unwind",
     description:
       "Whether it's a guided meditation, soothing soundscape, or deep breathing session, Ground helps you find your balance.",
-    angle: 10,
+    angle: 15,
   },
   {
     name: "Follow your progress",
     description:
       "Buildyour mindfulness habit with personalized steaks, mood tracking and insights to help you grow.",
-    angle: 30,
+    angle: 40,
   },
 ].map((item, idx) => ({ ...item, id: idx + 1 }));
 
 const PhoneShowcase = () => {
-  const [selectedIndex, setSelectedIndex] = useState<number | null>(null);
+  const [selectedIndex, setSelectedIndex] = useState(1);
 
   const handleClick = (index: number) => {
-    setSelectedIndex(index === selectedIndex ? null : index);
+    const newIdx = index + 1;
+    setSelectedIndex(newIdx);
   };
+
+  const getVisibleItems = () => {
+    switch (selectedIndex) {
+      case 1:
+        return items;
+      case 2:
+        return items.slice(1);
+      case 3:
+        return items.slice(2);
+      default:
+        return items;
+    }
+  };
+
+  const visibleItems = getVisibleItems();
+  const originalAngles = items.map((item) => item.angle);
 
   return (
     <MotionConfig transition={TRANSITION}>
@@ -74,7 +91,10 @@ const PhoneShowcase = () => {
             <div className="max-w-md flex flex-col gap-4">
               {items.map((item, idx) => (
                 <div
-                  className="p-4 rounded-lg hover:bg-primary group cursor-pointer bg-transparent duration-300 transition-all border border-dashed hover:border-transparent"
+                  className={cn(
+                    "p-4 rounded-lg hover:bg-primary group cursor-pointer bg-transparent duration-300 transition-all border border-dashed hover:border-transparent",
+                    selectedIndex === idx + 1 && "border-primary"
+                  )}
                   key={idx + item.name}
                   onClick={() => handleClick(idx)}
                 >
@@ -88,38 +108,36 @@ const PhoneShowcase = () => {
               ))}
             </div>
             <div className="flex-1 h-[600px] relative z-0 flex items-center justify-center">
-              <AnimatePresence>
-                {items.map((item, idx) => {
-                  if (selectedIndex === null || idx <= selectedIndex) {
-                    return (
-                      <m.div
-                        key={item.name}
-                        className={cn(
-                          "h-[600px] w-96 absolute bottom-0 top-0 left-0 right-20 m-auto",
-                          item.className
-                        )}
-                        animate={{
-                          rotate: item.angle,
-                          zIndex: items.length - idx,
-                          top: idx * 30,
-                          opacity: 1,
-                        }}
-                        initial={{ opacity: 0 }}
-                        exit={{ opacity: 0 }}
-                        style={{ transformOrigin: "bottom center" }}
-                      >
-                        <img
-                          src="/phone.png"
-                          alt="phone"
-                          className="size-full object-cover scale-110"
-                        />
-                      </m.div>
-                    );
-                  }
-                  return null;
+              <AnimatePresence mode="popLayout">
+                {visibleItems.map((item, idx) => {
+                  const newAngle = originalAngles[idx];
+
+                  return (
+                    <m.div
+                      key={item.name}
+                      className={cn(
+                        "h-[600px] w-96 absolute bottom-0 top-0 left- right-20 m-auto"
+                      )}
+                      animate={{
+                        rotate: newAngle,
+                        top: idx * 20,
+                        zIndex: items.length - idx,
+                        opacity: 1,
+                      }}
+                      initial={{ opacity: 0 }}
+                      exit={{ opacity: 0 }}
+                      style={{ transformOrigin: "bottom center" }}
+                    >
+                      <img
+                        src="/phone.png"
+                        alt="phone"
+                        className="size-full object-cover scale-110"
+                      />
+                    </m.div>
+                  );
                 })}
               </AnimatePresence>
-              <div className="h-80 w-full bg-gradient-to-b from-transparent via-background to-white absolute -bottom-20 left-0 z-50" />
+              <div className="h-80 w-[calc(100%+200px)] bg-gradient-to-b from-transparent via-background to-white absolute -bottom-20 left-0 z-50" />
             </div>
           </div>
         </div>
@@ -127,9 +145,17 @@ const PhoneShowcase = () => {
     </MotionConfig>
   );
 };
-
 const Header = () => {
-  return <header className="border-b border-dashed h-16"></header>;
+  return (
+    <header className="border-b border-dashed h-16">
+      <nav className="max-w-5xl mx-auto w-full flex items-center justify-between h-full">
+        <Logo gradient={false} className="size-12" />
+        <Waitlist>
+          <Button size="lg">Join waitlist</Button>
+        </Waitlist>
+      </nav>
+    </header>
+  );
 };
 
 export default PhoneShowcase;
