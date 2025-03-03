3651fea3b44fd8f3d0f8bd14bc3bcdbe515098f1
code-env <org.codeen@gmail.com>
2025-03-03 04:58:54 +0100

    feat: changed the logo main wrapper to a link

--- src/components/shared/logo.tsx
+++ src/components/shared/logo.tsx
@@ -1,18 +1,20 @@
 import { cn } from "@/lib/utils";
+import { Link } from "react-router";
 
 interface LogoProps {
   className?: string;
   gradient?: boolean;
+  path?: string;
 }
 
-const Logo = ({ className, gradient = true }: LogoProps) => {
+const Logo = ({ className, gradient = true, path }: LogoProps) => {
   return (
-    <div className={cn("size-20 relative", className)}>
+    <Link className={cn("size-20 relative", className)} to={path ?? "/"}>
       <img src="/Graound.svg" alt="Graound" className="size-full" />
       {gradient && (
         <div className="gradient absolute inset-0 -z-10 size-16 m-auto blur-2xl" />
       )}
-    </div>
+    </Link>
   );
 };
 
