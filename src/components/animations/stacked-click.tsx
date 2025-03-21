bfb8b677bc7ec27c89eb8056cc9a8c974fb1c06e
code-env <org.codeen@gmail.com>
2025-03-21 16:44:16 +0100

    feat: added some other components

--- src/components/animations/stacked-click.tsx
+++ src/components/animations/stacked-click.tsx
@@ -1,7 +1,7 @@
 import { cn } from "@/lib/utils";
 import { Check } from "lucide-react";
 import { Dispatch, SetStateAction, useState } from "react";
-import { motion as m } from "motion/react";
+import { motion as m, MotionConfig, Transition } from "motion/react";
 
 interface TimelineItem {
   date: string;
@@ -28,20 +28,27 @@ const timelineItems: TimelineItem[] = [
   },
 ];
 
+const TRANSITION: Transition = {
+  duration: 0.3,
+  ease: "circInOut",
+  // delay: 0.1,
+};
+
 const StackClick = () => {
   const [selected, setSelected] = useState(0);
   return (
-    <div className="full center">
-      <div className="max-w-lg border w-full flex items-center gap-10">
-        <div className="flex-1"></div>
-        <VerticalTimeline
-          items={timelineItems}
-          setSelected={setSelected}
-          selected={selected}
-        />
-        <div className="flex-1"></div>
+    <MotionConfig transition={TRANSITION}>
+      <div className="full center  z-0">
+        <div className="max-w-md border overflow-hidden w-full flex items-center gap-10">
+          <div className="flex-1 border size-full"></div>
+          <VerticalTimeline
+            items={timelineItems}
+            setSelected={setSelected}
+            selected={selected}
+          />
+        </div>
       </div>
-    </div>
+    </MotionConfig>
   );
 };
 
@@ -66,30 +73,46 @@ function VerticalTimeline({
           className="flex items-start cursor-pointer"
           onClick={() => setSelected(index)}
         >
-          {/* Timeline circle and line */}
-          <m.div
-            layoutId="active-timeline-indicator-container"
-            className="relative flex flex-col items-center"
-          >
-            <m.div
-              layoutId="active-timeline-indicator"
+          <div className="relative flex flex-col items-center">
+            <div
               className={cn(
-                "flex h-5 w-5 items-center justify-center rounded-full border-4",
-                selected === index
-                  ? "border-emerald-600 bg-emerald-600 text-white"
-                  : "border-gray-300 bg-white"
+                "flex h-5 relative w-5 items-center justify-center rounded-full border-4 border-gray-300 bg-white"
               )}
-            >
-              {selected === index && <Check className="h-3 w-3" />}
-            </m.div>
-            {/* Connector line */}
+            />
             {index < items.length - 1 && (
-              <div className="h-12 w-1 bg-gray-300" />
+              <div className="h-6 w-1 bg-gray-300" />
             )}
-          </m.div>
+            {selected === index && (
+              <m.div
+                layoutId="active-timeline-container"
+                className="absolute h-5 w-5 bg-emerald-600 rounded-full z-50"
+              >
+                <m.div
+                  className="size-full bg-emerald-500 rounded-full center text-white"
+                  layoutId="active-timeline-indicator-icon"
+                >
+                  <Check className="h-3 w-3" />
+                </m.div>
 
-          {/* Date text */}
-          <div className="ml-4 pb-10">
+                <div
+                  className={cn(
+                    "absolute h-8 left-0 right-0 mx-auto w-1 bg-gradient-to-b from-transparent via-emerald-600 to-transparent -z-10",
+                    {
+                      "top-0 bottom-0 my-auto h-16":
+                        index > 0 && index < items.length - 1,
+                      "top-0": index === 0,
+                      "bottom-0": index === items.length - 1,
+                    }
+                  )}
+                />
+              </m.div>
+            )}
+          </div>
+          <div
+            className={cn("ml-4", {
+              "pb-5": index < items.length - 1,
+            })}
+          >
             <span
               className={cn(
                 "text-sm font-medium",
