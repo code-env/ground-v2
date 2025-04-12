259c89ad81d0286eece997ba6697d4a70ee559b7
code-env <org.codeen@gmail.com>
2025-04-12 07:21:48 +0100

    fix: comment out unused imports and GitHub link in Hero component

--- src/components/shared/hero.tsx
+++ src/components/shared/hero.tsx
@@ -1,9 +1,10 @@
 import { motion } from "motion/react";
 import { buttonVariants } from "../ui/button";
 import Logo from "./logo";
-import { Icons } from "./icons";
-import { Star } from "lucide-react";
-import { siteConfig } from "@/lib/config";
+// import { Icons } from "./icons";
+// import { Star } from "lucide-react";
+// import { siteConfig } from "@/lib/config";
+
 const Hero = ({ hidden }: { hidden: boolean }) => {
   return (
     <div className="w-full md:border-b bg-background">
@@ -38,7 +39,7 @@ const Hero = ({ hidden }: { hidden: boolean }) => {
           </button>
         )}
 
-        <a
+        {/* <a
           href={siteConfig.link.githubRepo}
           target="_blank"
           className="absolute top-0 right-0 flex items-center gap-2 border border-r-0 border-t-0 border-muted-foreground/20 p-2 text-sm font-medium"
@@ -46,7 +47,7 @@ const Hero = ({ hidden }: { hidden: boolean }) => {
           <Icons.github className="w-4 h-4 fill-primary" />
           <span>Star on GitHub</span>
           <Star className="w-4 h-4 fill-primary/50" />
-        </a>
+        </a> */}
       </div>
     </div>
   );
